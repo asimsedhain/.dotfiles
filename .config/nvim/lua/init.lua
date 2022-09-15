fdc7aec813717c86a6c97f60bf097f7c8c248cce
asimsedhain <asimsedhain33@gmail.com>
2022-09-14 23:52:22 -0500

    refactor: move tree-sitter to plugin.lua

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,9 +1,7 @@
 -- require other modules
 
 require("sets")
-require("netrw")
 require("remap")
 require("plugin")
 require("color")
 require("lsp")
-require("tree-sitter")
--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -2,12 +2,10 @@
 local g = vim.g
 local map = require("utils").map
 
-
 -- variables to modify netrw
 g.netrw_banner = 0
 g.netrw_liststyle = 3
 g.netrw_winsize = 25
-
 
 -- variables to modify Vim Airline
 g["airline_section_y"] = ""
@@ -72,7 +70,41 @@ null_ls.setup({
 		null_ls.builtins.formatting.yamlfmt,
 	},
 })
---require("null-ls").setup({
---sources = {}
 
---})
+
+
+-- NVIM tree-sitter for highlighting 
+require 'nvim-treesitter.configs'.setup {
+	-- A list of parser names, or "all"
+	ensure_installed = { "cpp", "lua", "rust", "go", "javascript", "json", "markdown", "toml", "tsx", "typescript", "python",
+		"svelte", "css", "html" },
+
+	-- Install parsers synchronously (only applied to `ensure_installed`)
+	sync_install = false,
+
+	-- Automatically install missing parsers when entering buffer
+	auto_install = true,
+
+	-- List of parsers to ignore installing (for "all")
+	ignore_install = {},
+
+	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+	highlight = {
+		-- `false` will disable the whole extension
+		enable = true,
+
+		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+		-- the name of the parser)
+		-- list of language that will be disabled
+		disable = {},
+
+		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+		-- Using this option may slow down your editor, and you may see some duplicate highlights.
+		-- Instead of true it can also be a list of languages
+		additional_vim_regex_highlighting = false,
+	},
+}
