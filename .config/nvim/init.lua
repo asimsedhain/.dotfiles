90f68fb49d55487f8dae0986cd50cd2363ed02ce
asimsedhain <asimsedhain33@gmail.com>
2024-05-17 21:15:56 -0500

    fix: do not hot-reload lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -30,6 +30,11 @@ require("lazy").setup("plugins", {
 			lazy = "💤 ",
 		},
 	},
+	change_detection = {
+		-- automatically check for config file changes and reload the ui
+		enabled = false,
+		notify = false, -- get a notification when changes are found
+	},
 })
 
 -- require other modules
